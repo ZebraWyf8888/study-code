9dbed3fe1ebbd2dff905a4f0652240ef91ff35f4
wangyifan <453907683@qq.com>
2022-03-07 12:46:08 +0800

    hot

---
+++ src/main/java/aaaahot/三数之和.java
@@ -0,0 +1,94 @@
+package aaaahot;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class 三数之和 {
+    /**
+     * 三数之和，难度在去重
+     * @param nums
+     * @return
+     */
+    public static List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> ans = new ArrayList<>();
+        quickSort(nums,0,nums.length-1);
+        for (int i = 0; i < nums.length; i++) {
+
+            // 需要和上一次枚举的数不相同
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
+            int third = nums.length - 1;
+            int tar =  - nums[i];
+
+            for (int second = i + 1; second < nums.length; second++) {
+                if (second > i+1 && nums[second] == nums[second-1]) {
+                    continue;
+                }
+
+                // 需要保证 b 的指针在 c 的指针的左侧
+                while (second < third && nums[second] + nums[third] > tar) {
+                    --third;
+                }
+                
+                if (second == third){
+                    break;
+                }
+
+                if (nums[second] + nums[third] == tar) {
+                    List<Integer> list = new ArrayList<Integer>();
+                    list.add(nums[i]);
+                    list.add(nums[second]);
+                    list.add(nums[third]);
+                    ans.add(list);
+                }
+
+            }
+
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        int[] ints = {-1, 0, 1, 2, -1, -4};
+        List<List<Integer>> lists = threeSum(ints);
+
+
+    }
+    public static void quickSort(int[] num,int low,int high){
+        int i = low;
+        int j = high;
+        int temp;
+
+        if (i > j) {
+            return;
+        }
+        int k = num[low];
+
+        while (i < j){
+            while (num[j] >= k && i < j) {
+                j--;
+            }
+
+            while (num[i] <= k && i < j) {
+                i++;
+            }
+
+            if (i < j) {
+                temp = num[j];
+                num[j] = num[i];
+                num[i] = temp;
+            }
+        }
+
+        temp = num[low];
+        num[low] = num[i];
+        num[i] = temp;
+
+
+        quickSort(num,low,i-1);
+        quickSort(num,i+1,high);
+
+
+    }
+}
---
+++ src/main/java/aaaahot/岛屿的数量最大面积周长.java
@@ -0,0 +1,237 @@
+package aaaahot;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class 岛屿的数量最大面积周长 {
+
+    /**
+     * 岛屿数量，用0标识访问过，dfs
+     *
+     * @param grid
+     * @param r
+     * @param c
+     */
+    void dfs(char[][] grid, int r, int c) {
+        int nr = grid.length;
+        int nc = grid[0].length;
+
+        if (r < 0 || c < 0 || r >= nr || c >= nc || grid[r][c] == '0') {
+            return;
+        }
+
+        grid[r][c] = '0';
+        dfs(grid, r - 1, c);
+        dfs(grid, r + 1, c);
+        dfs(grid, r, c - 1);
+        dfs(grid, r, c + 1);
+    }
+
+    public int numIslands(char[][] grid) {
+        if (grid == null || grid.length == 0) {
+            return 0;
+        }
+
+        int nr = grid.length;
+        int nc = grid[0].length;
+        int num_islands = 0;
+        for (int r = 0; r < nr; ++r) {
+            for (int c = 0; c < nc; ++c) {
+                if (grid[r][c] == '1') {
+                    ++num_islands;
+                    dfs(grid, r, c);
+                }
+            }
+        }
+
+        return num_islands;
+    }
+
+    /**
+     * 岛屿面积 dfs
+     *
+     * @param grid
+     * @return
+     */
+    public int maxAreaOfIsland(int[][] grid) {
+        int ans = 0;
+        for (int i = 0; i != grid.length; ++i) {
+            for (int j = 0; j != grid[0].length; ++j) {
+                ans = Math.max(ans, dfs(grid, i, j));
+            }
+        }
+        return ans;
+    }
+
+    public int dfs(int[][] grid, int cur_i, int cur_j) {
+        if (cur_i < 0 || cur_j < 0 || cur_i == grid.length || cur_j == grid[0].length || grid[cur_i][cur_j] != 1) {
+            return 0;
+        }
+        grid[cur_i][cur_j] = 0;
+
+        int ans = 1;
+
+        ans += dfs(grid, cur_i - 1, cur_j);
+        ans += dfs(grid, cur_i + 1, cur_j);
+        ans += dfs(grid, cur_i, cur_j - 1);
+        ans += dfs(grid, cur_i, cur_j + 1);
+
+        return ans;
+    }
+
+    /**
+     * 岛屿数量，并查集
+     */
+    class Solution {
+        class UnionFind {
+            int count;
+            int[] parent;
+            int[] rank;
+
+            public UnionFind(char[][] grid) {
+                count = 0;
+                int m = grid.length;
+                int n = grid[0].length;
+                parent = new int[m * n];
+                rank = new int[m * n];
+                for (int i = 0; i < m; ++i) {
+                    for (int j = 0; j < n; ++j) {
+                        if (grid[i][j] == '1') {
+                            parent[i * n + j] = i * n + j;
+                            ++count;
+                        }
+                        rank[i * n + j] = 0;
+                    }
+                }
+            }
+
+            public int find(int i) {
+                if (parent[i] != i) parent[i] = find(parent[i]);
+                return parent[i];
+            }
+
+            public void union(int x, int y) {
+                int rootx = find(x);
+                int rooty = find(y);
+                if (rootx != rooty) {
+                    if (rank[rootx] > rank[rooty]) {
+                        parent[rooty] = rootx;
+                    } else if (rank[rootx] < rank[rooty]) {
+                        parent[rootx] = rooty;
+                    } else {
+                        parent[rooty] = rootx;
+                        rank[rootx] += 1;
+                    }
+                    --count;
+                }
+            }
+
+            public int getCount() {
+                return count;
+            }
+        }
+
+        public int numIslands(char[][] grid) {
+            if (grid == null || grid.length == 0) {
+                return 0;
+            }
+
+            int nr = grid.length;
+            int nc = grid[0].length;
+            int num_islands = 0;
+            UnionFind uf = new UnionFind(grid);
+            for (int r = 0; r < nr; ++r) {
+                for (int c = 0; c < nc; ++c) {
+                    if (grid[r][c] == '1') {
+                        grid[r][c] = '0';
+                        if (r - 1 >= 0 && grid[r - 1][c] == '1') {
+                            uf.union(r * nc + c, (r - 1) * nc + c);
+                        }
+                        if (r + 1 < nr && grid[r + 1][c] == '1') {
+                            uf.union(r * nc + c, (r + 1) * nc + c);
+                        }
+                        if (c - 1 >= 0 && grid[r][c - 1] == '1') {
+                            uf.union(r * nc + c, r * nc + c - 1);
+                        }
+                        if (c + 1 < nc && grid[r][c + 1] == '1') {
+                            uf.union(r * nc + c, r * nc + c + 1);
+                        }
+                    }
+                }
+            }
+
+            return uf.getCount();
+        }
+    }
+
+
+    /**
+     * 岛屿面积 广度优先
+     *
+     * @param grid
+     * @return
+     */
+    public int maxAreaOfIsland2(int[][] grid) {
+        int ans = 0;
+        for (int i = 0; i != grid.length; ++i) {
+            for (int j = 0; j != grid[0].length; ++j) {
+                int cur = 0;
+                Queue<Integer> queuei = new LinkedList<Integer>();
+                Queue<Integer> queuej = new LinkedList<Integer>();
+                queuei.offer(i);
+                queuej.offer(j);
+                while (!queuei.isEmpty()) {
+                    int cur_i = queuei.poll(), cur_j = queuej.poll();
+                    if (cur_i < 0 || cur_j < 0 || cur_i == grid.length || cur_j == grid[0].length || grid[cur_i][cur_j] != 1) {
+                        continue;
+                    }
+                    ++cur;
+                    grid[cur_i][cur_j] = 0;
+                    int[] di = {0, 0, 1, -1};
+                    int[] dj = {1, -1, 0, 0};
+                    for (int index = 0; index != 4; ++index) {
+                        int next_i = cur_i + di[index], next_j = cur_j + dj[index];
+                        queuei.offer(next_i);
+                        queuej.offer(next_j);
+                    }
+                }
+                ans = Math.max(ans, cur);
+            }
+        }
+        return ans;
+    }
+
+    static int[] dx = {0, 1, 0, -1};
+    static int[] dy = {1, 0, -1, 0};
+
+    /**
+     * 对于一个陆地格子的每条边，它被算作岛屿的周长当且仅当这条边为网格的边界或者相邻的另一个格子为水域。 因此，我们可以遍历每个陆地格子，看其四个方向是否为边界或者水域，如果是，将这条边的贡献（即 11）加入答案 \textit{ans}ans 中即可。
+     *
+     * 作者：LeetCode-Solution
+     * 链接：https://leetcode-cn.com/problems/island-perimeter/solution/dao-yu-de-zhou-chang-by-leetcode-solution/
+     * 来源：力扣（LeetCode）
+     * 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+     * @param grid
+     * @return
+     */
+    public int islandPerimeter(int[][] grid) {
+        int n = grid.length, m = grid[0].length;
+        int ans = 0;
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < m; ++j) {
+                if (grid[i][j] == 1) {
+                    int cnt = 0;
+                    for (int k = 0; k < 4; ++k) {
+                        int tx = i + dx[k];
+                        int ty = j + dy[k];
+                        if (tx < 0 || tx >= n || ty < 0 || ty >= m || grid[tx][ty] == 0) {
+                            cnt += 1;
+                        }
+                    }
+                    ans += cnt;
+                }
+            }
+        }
+        return ans;
+    }
+}
