95599ac735c4c41bdeb8ee943609cc834b7459b7
wangyifan <453907683@qq.com>
2022-04-16 14:57:47 +0800

    单词拆分 139. 单词拆分

---
+++ src/main/java/aaaahot/单词拆分.java
@@ -0,0 +1,28 @@
+package aaaahot;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class 单词拆分 {
+    public boolean wordBreak(String s, List<String> wordDict) {
+        Set<String> wordDictSet = new HashSet<>(wordDict);
+        int length = s.length();
+        Boolean[] booleans = new Boolean[length];
+        booleans[0] = Boolean.TRUE;
+        for (int i = 1; i < length; i++) {
+            for (int j = 0; j < i; j++) {
+                if (booleans[j] && wordDictSet.contains(s.substring(j,i))) {
+                    booleans[i] = Boolean.TRUE;
+                    break;
+                }
+            }
+        }
+        return booleans[length-1];
+    }
+//
+//    作者：LeetCode-Solution
+//    链接：https://leetcode-cn.com/problems/word-break/solution/dan-ci-chai-fen-by-leetcode-solution/
+//    来源：力扣（LeetCode）
+//    著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+}
