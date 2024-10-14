bb6f9769e000a1d747881b5ee6119ddd6f2bfead
Wyf <yf.wang@aftership.com>
2024-10-14 17:11:58 +0800

    feat: fix Code Red

---
+++ src/main/go/project-test/main.go
@@ -0,0 +1,20 @@
+package main
+
+//
+//import (
+//	"time"
+//
+//	"cloud.google.com/go/civil"
+//	"github.com/ThreeKing2018/goutil/array"
+//)
+//
+//func main() {
+//	of := civil.DateOf(time.Now().UTC())
+//	x := of.String()
+//	println(x)
+//
+//	inArray := array.InArray("aa", []string{
+//		"sdafa", "dsafsaf", "aa",
+//	})
+//	println(inArray)
+//}
